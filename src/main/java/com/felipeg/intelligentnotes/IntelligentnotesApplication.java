d29b6e190b8b3746f4af1f2706d5c1778a035bf6
Felipe Galeote <felipe10gale@gmail.com>
2021-06-01 22:49:51 -0300

    Creates the application and configures .gitignore

---
+++ src/main/java/com/felipeg/intelligentnotes/IntelligentnotesApplication.java
@@ -0,0 +1,13 @@
+package com.felipeg.intelligentnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentnotesApplication.class, args);
+	}
+
+}
