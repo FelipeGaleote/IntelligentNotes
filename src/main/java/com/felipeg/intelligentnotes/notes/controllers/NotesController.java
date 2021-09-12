d967558c7a044487b9e31523ab430b43caa59043
Felipe Fabri Galeote <felipe10gale@gmail.com>
2021-09-12 01:18:44 -0300

    Document operation responses and summary using OpenAPI
    The know success and error responses of the backend endpoints were documented using OpenAPI. Also added a description of each endpoint

--- src/main/java/com/felipeg/intelligentnotes/notes/controllers/NotesController.java
+++ src/main/java/com/felipeg/intelligentnotes/notes/controllers/NotesController.java
@@ -1,10 +1,17 @@
 package com.felipeg.intelligentnotes.notes.controllers;
 
+import com.felipeg.intelligentnotes.exceptions.ErrorResponse;
 import com.felipeg.intelligentnotes.notes.dtos.CreateNoteInput;
 import com.felipeg.intelligentnotes.notes.dtos.NoteOutput;
 import com.felipeg.intelligentnotes.notes.models.Note;
 import com.felipeg.intelligentnotes.notes.repositories.NotesRepository;
 import com.felipeg.intelligentnotes.users.models.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,6 +31,21 @@ public class NotesController {
     private NotesRepository notesRepository;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new note for the logged in user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Note created",
+                    content = {
+                            @Content(schema = @Schema(implementation = NoteOutput.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {
+                            @Content(schema = @Schema(hidden = true))
+                    })
+    })
     public ResponseEntity<NoteOutput> createNote(@RequestBody @Valid CreateNoteInput createNoteInput, Principal principal) {
         var note = new Note(createNoteInput.getTitle(), createNoteInput.getContent(), getUser(principal));
         note = notesRepository.save(note);
@@ -36,6 +58,21 @@ public class NotesController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "List the notes of the logged in user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listed notes",
+                    content = {
+                            @Content(array = @ArraySchema(schema = @Schema(implementation = NoteOutput.class)))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {
+                            @Content(schema = @Schema(hidden = true))
+                    })
+    })
     public ResponseEntity<List<NoteOutput>> listUserNotes(Principal principal) {
         var user = getUser(principal);
         List<Note> userNotes = notesRepository.findByUser(user);
--- src/main/java/com/felipeg/intelligentnotes/users/controllers/UserController.java
+++ src/main/java/com/felipeg/intelligentnotes/users/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.felipeg.intelligentnotes.users.controllers;
 
+import com.felipeg.intelligentnotes.exceptions.ErrorResponse;
 import com.felipeg.intelligentnotes.security.JwtTokenUtil;
 import com.felipeg.intelligentnotes.users.dtos.input.LoginInput;
 import com.felipeg.intelligentnotes.users.dtos.input.SignUpInput;
@@ -7,6 +8,12 @@ import com.felipeg.intelligentnotes.users.dtos.output.LoginOutput;
 import com.felipeg.intelligentnotes.users.dtos.output.SignUpOutput;
 import com.felipeg.intelligentnotes.users.models.User;
 import com.felipeg.intelligentnotes.users.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -42,6 +49,24 @@ public class UserController {
     }
 
     @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Authenticates the user with username and password and returns JWT token on header")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User authenticated",
+                    content = {
+                            @Content(schema = @Schema(implementation = LoginOutput.class))
+                    },
+                    headers = {
+                            @Header(name = "Authorization", description = "JWT token that must be used to do authenticated requests")
+                    }),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "401", description = "Failed to authenticate user",
+                    content = {
+                            @Content(schema = @Schema(hidden = true))
+                    })
+    })
     public ResponseEntity<LoginOutput> login(@RequestBody @Valid LoginInput request) {
 
         try {
@@ -59,6 +84,17 @@ public class UserController {
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Creates a new user account with the information provided on request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created",
+                    content = {
+                            @Content(schema = @Schema(implementation = SignUpOutput.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
     public ResponseEntity<SignUpOutput> signUp(@RequestBody @Valid SignUpInput signUpInput) {
         var user = new User();
         user.setUsername(signUpInput.getUsername());
